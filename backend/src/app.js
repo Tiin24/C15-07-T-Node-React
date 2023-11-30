44ed6688d301486766669e0f226de53c94a9a979
David Castro <davidernesto.castroanaya@gmail.com>
2023-11-30 13:52:13 -0600

    19 backend creacion y configuracion del modelo de usuario (#20)
    * feat(backend): user model added, controllers, services, auth and role middlewares
    
    * fix(backend): user.services file added

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,34 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const {port} = require('./config');
+const db = require('./utils/database');
+
+const userRouter = require('./users/users.router');
+const authRouter = require('./auth/auth.router');
 
 
 app.use(express.json());
 app.use(cors())
+
+db.authenticate()
+    .then(()=> {
+        console.log('Database Authenticated')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+db.sync()
+    .then(()=> {
+        console.log('Database Synced')
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/auth', authRouter)
 
 
 
