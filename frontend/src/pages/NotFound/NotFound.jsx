8afc3a98fa331035627bda91f0ccb7e5433b19e8
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-12-06 02:07:10 +0100

    feat(frontend): not found page

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import NavBar from '../../components/NavBar/NavBar';
+import { Link } from 'react-router-dom';
+import notFound from '../../assets/notfound.svg';
 
 function NotFound() {
   return (
     <>
-      <div className='grow bg-[#f8f8f8]'>Error404</div>
+      <div className='flex grow items-center justify-center bg-[#f8f8f8]'>
+        <div className='text-center'>
+          <div className='flex items-center justify-center'>
+            <img className='w-40 sm:w-60 lg:w-96' src={notFound} alt='' />
+          </div>
+          <p className='pb-6 '>La página que estás buscando no existe.</p>
+          <Link to={'/'} className='btn btn-primary'>
+            Volver al inicio
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
