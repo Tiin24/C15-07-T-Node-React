dd12c206f4fd8ef10b7cb80635f95a63170e9bb1
Alberto Gómez Juan <agomezjuan@hotmail.com>
2023-12-06 01:18:22 +0100

    feat(frontend): navBar template

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
 
 function NotFound() {
-  return <div>Error404</div>;
+  return (
+    <>
+      <div className='grow bg-[#f8f8f8]'>Error404</div>
+    </>
+  );
 }
 
 export default NotFound;
